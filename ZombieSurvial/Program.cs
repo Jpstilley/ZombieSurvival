7a39d045f3af895d78bb378eeb87b54029a78e47
Jpstilley <joey.stilley@gmail.com>
2021-07-30 13:48:10 -0500

    Added Project to Git

---
+++ ZombieSurvial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ZombieSurvial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Survivor("gun");
+            var jack = new Survivor("bat");
+            var zombie = new Zombie();
+        
+            john.PickUpEquip("knife");
+            foreach (string equ in john.Equipment)
+            {
+                Console.WriteLine(equ);
+            }
+            john.Attack(zombie);
+            Console.WriteLine(john.Reputation);
+        }
+    }
+}
---
+++ ZombieSurvial/Survivor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZombieSurvial 
+{
+    class Survivor : ITargetable
+    {
+        public List<string> Equipment { get; set; } = new List<string>();
+        public int Reputation { get; private set; } = 100;
+        public bool IsZombie { get; } =  false;
+
+
+        public Survivor(string startingEquipment)
+        {
+            Equipment.Add(startingEquipment);
+        }
+
+        public void PickUpEquip(string newEquipment)
+        {
+            Equipment.Add(newEquipment);
+        }
+
+
+       
+        public void Attack(ITargetable attackee)
+        {
+            if(!attackee.IsZombie)
+            {
+                Reputation = Reputation - 10;
+            }
+            
+        }
+    }
+}
